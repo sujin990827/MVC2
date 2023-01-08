6f6b31f114b9494ff6969b414fbd6f73b9ef733d
sujin lim <swuujin990827@gmail.com>
2023-01-08 22:22:28 +0900

    feat: From 전송 객체 분리

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,27 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+
+	MessageCodesResolver messageCodesResolver = new DefaultMessageCodesResolver();
+
+	@Test
+	void messageCodesResolverObject(){
+		String[] messageCodes = messageCodesResolver.resolveMessageCodes("required", "item");
+		Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+	}
+
+	@Test
+	void messageCodesResolverField(){
+		String[] messageCodes = messageCodesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+		Assertions.assertThat(messageCodes).containsExactly("required.item.itemName",
+			"required.itemName",
+			"required.java.lang.String",
+			"required");
+	}
+}
